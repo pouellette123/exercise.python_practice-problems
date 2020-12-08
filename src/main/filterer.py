08c524f0fc05b1c499257fcdae26442f2010d0cd
Paul Ouellette <pouellette123es@gmail.com>
2020-12-08 14:29:03 -0500

    added code for main

--- src/main/filterer.py
+++ src/main/filterer.py
@@ -1,33 +1,33 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Filterer(object):
     def remove_characters(self, string_to_remove_from, characters_to_remove):
-        ''' TODO - Implement solution
-        Given 2 arguments:
-            `string_to_remove_from`, representative of the string to remove the characters from
-            `characters_to_remove`, representative of the string of characters that should be removed from the `string_to_remove_from`
-        return
-            (case sensitive)
-            a string with each character in the `characters_to_remove` removed from the string
-        '''
-        return None
+
+        remove_list = [character for character in characters_to_remove]
+        rstring = ""
+        for char in string_to_remove_from:
+            if char not in remove_list:
+                rstring += char
+
+        return rstring
 
     def remove_vowels(self, string_to_remove_from):
-        ''' TODO - Implement solution
-        Given 1 arguments:
-            `string_to_remove_from`, representative of the string to remove the characters from
-        return
-            (case insensitive)
-            a string with each character in the list `['a','e',i','o','u']` removed from the string
-        '''
-        return None
+        vowel_list = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        rstring = ""
+        for char in string_to_remove_from:
+            if char not in vowel_list:
+                rstring += char
+
+        return rstring
 
     def remove_consonants(self, string_to_remove_from):
-        ''' TODO - Implement solution
-        Given 1 arguments:
-            `string_to_remove_from`, representative of the string to remove the characters from
-        return
-            (case insensitive)
-            a string with each character in the list `['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']` removed from the string
-        '''
-        return None
+        constants_list = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', \
+                      'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', \
+                      'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', \
+                      'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
+                      ]
+        rstring = ""
+        for char in string_to_remove_from:
+            if char not in constants_list:
+                rstring += char
 
+        return rstring
--- src/main/lister.py
+++ src/main/lister.py
@@ -1,6 +1,10 @@
 # Created by Leon Hunter at 12:10 PM 12/08/2020
 class Lister(object):
     def get_integer_list(self, start, stop, step):
+        if step==0:
+            return [0]
+        else:
+            return [num for num in range(start,stop+1,step)]
         ''' TODO - Implement solution
         Given 3 integers,
             `start`, `stop`, and `step`
@@ -9,9 +13,12 @@ class Lister(object):
             incrementing by `step`,
             up to and including `stop`
         '''
-        return None
 
     def get_even_list(self, start, stop, step):
+        if step==0:
+            return [0]
+        else:
+            return [num for num in range(start, stop+1, step) if num%2 == 0]
         ''' TODO - Implement solution
         Given 3 integers,
             `start`, `stop`, and `step`
@@ -21,10 +28,13 @@ class Lister(object):
             up to and including `stop`
             and are divisible by 2
         '''
-        return None
 
     def get_odd_list(self, start, stop, step):
-        ''' TODO - Implement solution
+        if step == 0:
+            return []
+        else:
+            return [num for num in range(start, stop+1, step) if num % 2 != 0]
+    ''' TODO - Implement solution
         Given 3 integers,
             `start`, `stop`, and `step`
         return a list of all integers
@@ -33,4 +43,3 @@ class Lister(object):
             up to and including `stop`
             and are not divisible by 2
         '''
-        return None
